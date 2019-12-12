bbc000978ac616ed6235a2860fd542e4cc3f73df
Tony Guan <tonyg06@msn.com>
2019-12-12 12:19:25 +1100

    More code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 
 let currentPlayer = 'X';
-
-
-//1. Make event listeners for 1-9 and button
-
-//2. if statements
-
-// 1, 2, 3 ==> win,  4, 5, 6 ==> win, 7, 8, 9 ==> win
-// 1, 4, 7 ==> win, 2, 5, 8 ==> win, 3, 6, 9 ==> win
-// 1, 5, 9 ==> win, 3, 5, 7 ==> win
-
+let tilesFilled = 0;
 
 // write a on.('click') function that logs what square has been fixed
 
@@ -19,9 +10,11 @@ let currentPlayer = 'X';
 // document.querySelector('button').addEventListener('click', handleClick);
 
 //Congratulate the winner
-function congratulateWinner(player) {
+function congratulateWinner() {
   setTimeout(function() {
-    alert("Congratulation! You won!");
+    alert(`Congratulation! ${currentPlayer} won!`);
+    clearBoard();
+    tilesFilled = 0;
   }, 200);
 }
 
@@ -42,41 +35,61 @@ for(let i = 0; i < boxes.length; i++ ) {
     // which box actually just got clicked (.target)
 
     const clickedBox = event.target;
+    if (clickedBox.innerHTML !== ""){
+      return;
+    }else{
+      clickedBox.innerHTML = currentPlayer;
+    }
 
-    console.log('clicked!', clickedBox.id );
+    // console.log('clicked!', clickedBox.id );
 
     // Set the box contents to show the player's symbol
-    clickedBox.innerText = currentPlayer;
 
-    // Has someone won the game
+    // Check who wins the game
 
     if( boxes[0].innerHTML === boxes[1].innerHTML && boxes[1].innerHTML === boxes[2].innerHTML && boxes[0].innerHTML === currentPlayer ) {
       congratulateWinner(currentPlayer);
+      return;
     }
 
     else if( boxes[3].innerHTML === boxes[4].innerHTML && boxes[4].innerHTML === boxes[5].innerHTML && boxes[3].innerHTML === currentPlayer ) {
       congratulateWinner(currentPlayer);
+      return;
     }
 
     else if( boxes[6].innerHTML === boxes[7].innerHTML && boxes[7].innerHTML === boxes[8].innerHTML && boxes[8].innerHTML === currentPlayer ) {
       congratulateWinner(currentPlayer);
+      return;
     }
     else if( boxes[0].innerHTML === boxes[3].innerHTML && boxes[3].innerHTML === boxes[6].innerHTML && boxes[0].innerHTML === currentPlayer ) {
       congratulateWinner(currentPlayer);
+      return;
     }
     else if( boxes[1].innerHTML === boxes[4].innerHTML && boxes[4].innerHTML === boxes[7].innerHTML && boxes[1].innerHTML === currentPlayer ) {
       congratulateWinner(currentPlayer);
+      return;
     }
     else if( boxes[2].innerHTML === boxes[5].innerHTML && boxes[5].innerHTML === boxes[8].innerHTML && boxes[2].innerHTML === currentPlayer ) {
       congratulateWinner(currentPlayer);
+      return;
     }
     else if( boxes[0].innerHTML === boxes[4].innerHTML && boxes[4].innerHTML === boxes[8].innerHTML && boxes[0].innerHTML === currentPlayer ) {
       congratulateWinner(currentPlayer);
+      return;
     }
     else if( boxes[2].innerHTML === boxes[4].innerHTML && boxes[4].innerHTML === boxes[6].innerHTML && boxes[2].innerHTML === currentPlayer ) {
       congratulateWinner(currentPlayer);
+      return;
     }
 
+//Check for draw
+    tilesFilled++;
+    if (tilesFilled === 9) {
+      alert(`It's a draw!`);
+      clearBoard();
+      tilesFilled = 0;
+      return;
+    }
 
 
     // Swap the player from one to the other
@@ -87,35 +100,54 @@ for(let i = 0; i < boxes.length; i++ ) {
       currentPlayer = 'X';
     }
 
-
+boxes[i].addEventListener
 
   });
 
 } // end of for loop
+
+const restart = document.querySelector('#playAgain');
+
+//clear the board
+const clearBoard = function(){
+  for ( let i = 0; i < boxes.length; i ++ ) {
+
+    boxes[i].innerHTML = '';
+
+  }
+};
+
+restart.addEventListener('click', function(){
+
+clearBoard();
+
+});
+
+
 
 
 
 //
 // //
 //
-const poem ={
-
-  book1: {
-          author: "Robert Frost",
-          datePublished: 1916,
-          name: "The Road Not Taken",
-          famousLine: "Two road diverged in a yellow wood"
-        },
-
-  book2: {
-          author: "Will Frost",
-          datePublished: 1936,
-          name: "King",
-          famousLine: "Wonderful Wonderful"
-          }
-
-};
+// const poem ={
 //
+//   book1: {
+//           author: "Robert Frost",
+//           datePublished: 1916,
+//           name: "The Road Not Taken",
+//           famousLine: "Two road diverged in a yellow wood"
+//         },
+//
+//   book2: {
+//           author: "Will Frost",
+//           datePublished: 1936,
+//           name: "King",
+//           famousLine: "Wonderful Wonderful"
+//           }
+//
+// };
+// //
 //
 // console.log(poem.famousLine);
 //
