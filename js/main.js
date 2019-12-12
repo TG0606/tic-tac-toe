ed146410a166ca3601dc70ded82de8e8809bb929
Tony Guan <tonyg06@msn.com>
2019-12-12 16:36:09 +1100

    winner message

--- js/main.js
+++ js/main.js
@@ -1,52 +1,46 @@
 
 let currentPlayer = 'X';
-let tilesFilled = 0;
-
-// write a on.('click') function that logs what square has been fixed
-
-// const handleClick = function(){
-//    console.log('hello!');
-// };
-// document.querySelector('button').addEventListener('click', handleClick);
+let boxesfilled = 0;
 
 //Congratulate the winner
+
 function congratulateWinner() {
+  // debugger;
+  const green = document.querySelector('.firstColor');
+  const black = document.querySelector('.secondColor');
+  const white = document.querySelector('.thirdColor');
   setTimeout(function() {
-    alert(`Congratulation! ${currentPlayer} won!`);
+    green.innerHTML = `Congratulations! `
+    black.innerHTML = `${currentPlayer} `
+    white.innerHTML = `won!`
     clearBoard();
-    tilesFilled = 0;
-  }, 200);
+    boxesfilled = 0;
+  }, 1000);
+  setTimeout(function() {
+  green.innerHTML = ` `;
+  black.innerHTML = ` `;
+  white.innerHTML = ` `;
+},2300);
 }
 
-// Get every TD tag from the page as an array of DOM nodes,
-// and save into the variable 'boxes'
+// Get all td tags from the DOM and save as a variable 'boxes'
 const boxes = document.querySelectorAll('td');
 
-
-// Loop over every DOM node in the boxes array,
-// and add an event listen function to each of them
+// Loop over every DOM node in the boxes array
 for(let i = 0; i < boxes.length; i++ ) {
-  // console.log( boxes[i] );
 
-  boxes[i].addEventListener('click', function( event ){
-    // This code runs whenever any of the boxes is clicked.
-    // We can make use of the 'event' argument passed in to
-    // our handler function by the browser, to find out
-    // which box actually just got clicked (.target)
+// Add an event listen function to each of them
+  boxes[i].addEventListener('click', function(){
 
-    const clickedBox = event.target;
-    if (clickedBox.innerHTML !== ""){
-      return;
+// Check if box is empty before trying to write in it
+
+    if (this.innerHTML !== ""){
+      return; //exit function if the tile is not empty
     }else{
-      clickedBox.innerHTML = currentPlayer;
+      this.innerHTML = currentPlayer;
     }
 
-    // console.log('clicked!', clickedBox.id );
-
-    // Set the box contents to show the player's symbol
-
     // Check who wins the game
-
     if( boxes[0].innerHTML === boxes[1].innerHTML && boxes[1].innerHTML === boxes[2].innerHTML && boxes[0].innerHTML === currentPlayer ) {
       congratulateWinner(currentPlayer);
       return;
@@ -82,25 +76,21 @@ for(let i = 0; i < boxes.length; i++ ) {
       return;
     }
 
-//Check for draw
-    tilesFilled++;
-    if (tilesFilled === 9) {
+//Check for the draw
+    boxesfilled++;
+    if (boxesfilled === 9) {
       alert(`It's a draw!`);
       clearBoard();
-      tilesFilled = 0;
+      boxesfilled = 0;
       return;
     }
 
-
-    // Swap the player from one to the other
+    // Change the player from one to the other
     if( currentPlayer === 'X' ){
       currentPlayer = 'O';
     } else {
-      // It was 'O', so switch to 'X'
-      currentPlayer = 'X';
+      currentPlayer = 'X'; // It was 'O', so change to 'X'
     }
-
-boxes[i].addEventListener
 
   });
 
